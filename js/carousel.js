b239dea8f5e830ffc30cbd16498545724487bce8
torresgol10 <torresgol10.it@gmail.com>
2018-07-08 23:18:26 +0200

    Correcion del archivo .js y quitar codigo js del html

--- js/carousel.js
+++ js/carousel.js
@@ -2,37 +2,82 @@ $(document).ready(function(){
   //Activacion del slider
   $('.carousel').carousel();
 
-  console.log($(".carousel.lazy picture source"));
-
-  //Funcion complementaria a la funcion de abajo para que no solo carge a la primera
-  //Siempre carga la siguiente imagen que tenga que servir
-  var lazySlider = function(numImg=0) {
-    var imgs= $('img.lazy-load');
-    for (var i = 0; i < numImg; i++) {
-      var src= $(imgs[i]).attr("data-src");
-      console.log(src);
-      $(imgs[i]).attr("src",src);
-      $(imgs[i]).removeAttr("data-src");
-      $(imgs[i]).removeClass("lazy-load");
+  // Get IE 10 and 11
+  var version = detectIE();
+    if(version){
+    //Funciones para el lazy load cuando son img simple
+    //Funcion complementaria a la funcion de abajo para que no solo carge a la primera
+    //Siempre carga la siguiente imagen que tenga que servir
+    var lazySlider = function(numImg) {
+      var src;
+      var imgs = $('img.lazy-load');
+      if(imgs.length > 0){
+        if(numImg > imgs.length){
+          numImg=imgs.length;
+        }
+        for (var i = 0; i < numImg; i++) {
+          src = $(imgs[i]).attr("data-src");
+          $(imgs[i]).attr("src",src);
+          $(imgs[i]).removeAttr("data-src");
+          $(imgs[i]).removeClass("lazy-load");
+        }
+      }
     }
-  }
-  //Llamada a la funcion de arriba para que la primera vez que cargue el slider carge la siguiente imagen
-  //El parametro es el numero de imagenes que se quiere precargar sin contar en la que esta ahora mismo
-  lazySlider(1);
-  //Evento para cuando empieze la transicion se carge la siguiente imagen que tenga que mostrar
-  //El evento se activa cuando empieze, tambien se puede poner cuando acabe
-  $('.carousel.lazy').on('slide.bs.carousel', function () {
-    //Precarga de 1 siempre que haya un evento de mover una imagen
+    //Llamada a la funcion de arriba para que la primera vez que cargue el slider carge la siguiente imagen
+    //El parametro es el numero de imagenes que se quiere precargar sin contar en la que esta ahora mismo
     lazySlider(1);
-  })
-  //Funcion que habilita el lazy load automaticamente cuando salta el evento de transicion del slider
-  //Solo carga la primera imagen
-  $(function() {
-    return $(".carousel.lazy").on("slide.bs.carousel", function(ev) {
-      var lazy;
-      lazy = $(ev.relatedTarget).find("img[data-src]");
-      lazy.attr("src", lazy.data('src'));
-      lazy.removeAttr("data-src");
+    //Evento para cuando empieze la transicion se carge la siguiente imagen que tenga que mostrar
+    //El evento se activa cuando empieze, tambien se puede poner cuando acabe
+    $('.carousel.lazy').on('slide.bs.carousel', function () {
+      //Precarga de 1 siempre que haya un evento de mover una imagen
+      lazySlider(1);
     });
-  });
+  }else{
+    /*
+    Funciones para el lazy load cuando tiene picture y source
+    */
+    var lazySliderSource = function(numImg) {
+      var imgs = $(".carousel.lazy picture > source[data-srcset]").parent();
+      var dataSrcset;
+      var source;
+      if(imgs.length > 0){
+        if(numImg > imgs.length){
+          numImg=imgs.length;
+        }
+        for (var z = 0; z < numImg; z++) {
+          source=$(imgs[z]).children("source");
+          for (var i = 0; i < source.length; i++) {
+            dataSrcset=$(source[i]).attr("data-srcset");
+            $(source[i]).attr("srcset",dataSrcset);
+            $(source[i]).removeAttr("data-srcset");
+          }
+        }
+      }
+    }
+    lazySliderSource(1);
+    $('.carousel.lazy').on('slide.bs.carousel', function () {
+      lazySliderSource(1);
+    });
+  }
+
 });
+function detectIE() {
+  var ua = window.navigator.userAgent;
+  // IE 10
+  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+  // IE 11
+  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+  // other browser
+  return false;
+}
