85b734248b5e11654fbc782c1533eb6170491ab9
torresgol10 <torresgol10.it@gmail.com>
2018-07-15 15:17:37 +0200

    Soporte para multislider en la misma pagina, ademas añadimos demo de demostracion

--- js/jquery-lazyload.js
+++ js/jquery-lazyload.js
@@ -1,33 +1,52 @@
 function carouselLazyLoad(numImg) {
   // Get IE 10 and 11
-  if(numImg==null){
-    numImg=1;
+  if(numImg == null){
+    numImg = 1;
   }
+  var id;
   var version = detectIE();
   var imgJPG = $(".carousel.lazy picture");
+  var multiSlider = $(".carousel.lazy");
 
   if(version || imgJPG.length == 0){
-    lazySlider(numImg);
-    lazySliderLast();
-    //Evento para cuando empieze la transicion se carge la siguiente imagen que tenga que mostrar
-    //El evento se activa cuando empieze, tambien se puede poner cuando acabe
+    for (var i = 0; i < multiSlider.length; i++) {
+      if(multiSlider.length == 1){
+        id = ".carousel";
+      }else{
+        id = "#"+$(multiSlider[i]).attr("id")+".carousel";
+      }
+      lazySlider(numImg,id);
+      lazySliderLast(id);
+    }
+      //Evento para cuando empieze la transicion se carge la siguiente imagen que tenga que mostrar
+      //El evento se activa cuando empieze, tambien se puede poner cuando acabe
     $('.carousel.lazy').on('slide.bs.carousel', function (ev) {
       //Precarga de 1 siempre que haya un evento de mover una imagen
+      id = "#"+$(this).attr("id")+".carousel";
+
       if(ev.direction=="right"){
-        lazySliderLast();
+        lazySliderLast(id);
       }else{
-        lazySlider(1);
+        lazySlider(1,id);
       }
     });
   }else{
-    lazySliderSource(numImg);
-    lazySliderSourceLast();
+    for (var i = 0; i < multiSlider.length; i++) {
+      if(multiSlider.length == 1){
+        id = ".carousel";
+      }else{
+        id = "#" + $(multiSlider[i]).attr("id") + ".carousel";
+      }
+      lazySliderSource(numImg,id);
+      lazySliderSourceLast(id);
+    }
     $('.carousel.lazy').on('slide.bs.carousel', function (ev) {
       //Llamada para que carge la foto hacia la que va
+      id = "#" + $(this).attr("id") + ".carousel";
       if(ev.direction=="right"){
-        lazySliderSourceLast();
+        lazySliderSourceLast(id);
       }else{
-        lazySliderSource(1);
+        lazySliderSource(1,id);
       }
     });
   }
@@ -36,9 +55,9 @@ function carouselLazyLoad(numImg) {
 //Funciones para el lazy load cuando son img simple
 //Funcion complementaria a la funcion de abajo para que no solo carge a la primera
 //Siempre carga la siguiente imagen que tenga que servir
-var lazySlider = function(numImg) {
+var lazySlider = function(numImg,id) {
   var src;
-  var imgs = $('img.lazy-load');
+  var imgs = $(id + '.lazy img.lazy-load');
   if(imgs.length > 0){
     if(numImg > imgs.length){
       numImg=imgs.length;
@@ -51,9 +70,9 @@ var lazySlider = function(numImg) {
     }
   }
 }
-var lazySliderLast = function() {
+var lazySliderLast = function(id) {
   var src;
-  var imgs = $('img.lazy-load');
+  var imgs = $(id+'.lazy img.lazy-load');
   if(imgs.length > 0){
     var lastImg=imgs.length-1;
 
@@ -69,10 +88,10 @@ var lazySliderLast = function() {
 /*
 Funciones para el lazy load cuando tiene picture y source
 */
-var lazySliderSource = function(numImg) {
-  var imgs = $(".carousel.lazy picture > source[data-srcset]").parent();
-  var dataSrcset;
-  var source;
+var lazySliderSource = function(numImg,id) {
+  var imgs = $(id + ".lazy picture > source[data-srcset]").parent();
+  var dataSrcset="";
+  var source="";
   if(imgs.length > 0){
     if(numImg > imgs.length){
       numImg=imgs.length;
@@ -88,10 +107,10 @@ var lazySliderSource = function(numImg) {
   }
 }
 
-var lazySliderSourceLast  = function() {
-  var imgs = $(".carousel.lazy picture > source[data-srcset]").parent();
-  var dataSrcset;
-  var source;
+var lazySliderSourceLast  = function(id) {
+  var imgs = $(id + ".lazy picture > source[data-srcset]").parent();
+  var dataSrcset="";
+  var source="";
   if(imgs.length > 0){
     var lastImg=imgs.length-1;
     for (var y = lastImg; y < imgs.length; y++) {
